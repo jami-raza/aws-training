b3b4afe39ccf23454e53dd2a07a02552af966a88
jami-raza <muhammadjami45@gmail.com>
2020-11-26 13:21:41 +0500

    add new folder

--- bin/todo-app.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { TodoAppStack } from '../lib/todo-app-stack';
-
-const app = new cdk.App();
-new TodoAppStack(app, 'TodoAppStack');
