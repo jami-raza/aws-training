ad9052d7838d4832a2a151981c1730b27e102f99
jami-raza <muhammadjami45@gmail.com>
2020-11-26 13:12:26 +0500

    workshop completed

--- lib/todo-app-stack.ts
+++ lib/todo-app-stack.ts
@@ -1,9 +1,29 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
+import { HitCounter } from './hit-counter';
+import {TableViewer} from 'cdk-dynamo-table-viewer';
 
 export class TodoAppStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,    // execution environment
+      code: lambda.Code.fromAsset('lambda-function'),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
+    });
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
+    });
+    new TableViewer(this, 'ViewHitCounter', {
+      title:'Hello table view',
+      table: helloWithCounter.table
+    })
   }
 }
